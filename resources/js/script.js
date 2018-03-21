6695d678076a2841e67949aee2412c2be0f27f8d
RANDOLF RESURRECCION <randolfresurreccion@RANDOLFs-MacBook-Pro.local>
2018-03-21 11:38:06 -0700

    added waypoint on sticky nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,12 @@
-$(document).ready(function() {
-    $("h1").click(function() {
-        $(this).css("background-color", "#ff0000");
-    });
+$(document).ready(function () {
+
+    $(".js--section-features").waypoint(function (direction) {
+        if (direction == "down") {
+            $("nav").addClass("sticky");
+        }
+        else {
+            $("nav").removeClass("sticky");
+        }
+    },
+        { offset: "60px" });
 });
